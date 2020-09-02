598541e527d56ffd9efe760b1fd6111883f2bdaf
CandyDaniele <daniele_oli_lucas@hotmail.com>
2020-09-02 15:07:50 -0300

    teste heroku

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const app = express();
 
 app.use(express.json());
 app.use("/account", accountRouter);
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 app.listen(3000, () => console.log('Api iniciada'));
